1940cdc0bf98f72c625e4999ffcc653f7a40f2b3
EC2 Default User <ec2-user@ip-172-31-25-211.ap-northeast-1.compute.internal>
2021-10-17 11:13:31 +0000

    add login

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-  <a href="/" class="navbar-brand">TODO App</a>
+  <a href="/" class="navbar-brand">NOTTODO App</a>
   <button
     class="navbar-toggler"
     type="button"
@@ -11,4 +11,22 @@
   >
     <span class="navbar-toggler-icon"></span>
   </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+       <ul class="navbar-nav ml-auto">
+           @auth
+           <li class="nav-item">
+               <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+               <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                   @csrf
+           </li>
+           @else
+          <li class="nav-item">
+               <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+          </li>
+          <li class="nav-item">
+               <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+           </li>
+          @endauth
+       </ul>
+  </div>
 </header>
